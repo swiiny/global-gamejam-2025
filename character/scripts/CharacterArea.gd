6bdf02dcc74c4098effcc0cc739a5df6a8fa96cb
maxime <maxime@genohm.com>
2025-01-21 19:23:08 +0100

    Adapt character speed based on anolog joystick input strength

--- character/scripts/CharacterArea.gd
+++ character/scripts/CharacterArea.gd
@@ -12,35 +12,45 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-		
-	position += velocity * delta
+	# The direction of the movement (4-axis)
+	var direction = _player_direction()  
+	# The speed of the movement (0-1 based on joystick inclination)
+	var velocity = Input.get_vector("ui_left", "ui_right","ui_up", "ui_down").length()
+	
+	position += direction * velocity * speed * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about boolean assignment
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+	_animate(direction)
 		
 		
 func _on_Player_body_entered(body):
 	hide()  # Player disappears after being hit.
 	emit_signal("hit")
+	
+func _player_direction() -> Vector2:
+	var direction = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1
+	return direction
+	
+func _animate(direction: Vector2) -> void:
+	if direction.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	if direction.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about boolean assignment
+		$AnimatedSprite2D.flip_h = direction.x < 0
+	elif direction.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = direction.y > 0
