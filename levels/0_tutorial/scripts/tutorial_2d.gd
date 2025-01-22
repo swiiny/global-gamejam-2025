bbb8418b3994c445a220d7242df6cd5bb56fced3
Jeremy Theintz <jeremy@appogee.ch>
2025-01-22 19:24:12 +0100

    chore: the tutorial level tilemap is done

--- levels/0_tutorial/scripts/tutorial_2d.gd
+++ levels/0_tutorial/scripts/tutorial_2d.gd
@@ -1,22 +1,37 @@
 extends Node2D
 
+# the room the user is in
 var current_room = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# make the user span at SpawnMarker2D location
-	$Character2d.position = $SpawnMarker2D.position
+	_spawn()	
+	_set_camera_limits()
+	_set_events()
 
+# make the user span at SpawnMarker2D location
+func _spawn():
+	$Character2d.position = $SpawnMarker2D.position
+	
+# set the camera limits for this level
+func _set_camera_limits(): 
+	var camera = $Character2d/Player/Camera2D
+	
+	camera.limit_right = 34 * 64
+	camera.limit_top = -1 * 64
+	camera.limit_bottom = 24 * 64
+
+func _set_events():
 	# Connect `body_entered` signals for all rooms
 	for room in $Rooms.get_children():
 		# the area 2D of the room node
 		var area = room.get_node("Area2D")
 		area.connect("body_entered", Callable(self, "_on_area_body_entered").bind(room))
-
-
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#print(current_room)
+	print($Character2d/Player.position)
 	pass
 
 
