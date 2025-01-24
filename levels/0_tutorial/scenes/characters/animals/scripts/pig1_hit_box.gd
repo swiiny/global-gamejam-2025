1fd8b442bfec20a8a15c27d3da30b6858b1d1520
Jeremy Theintz <jeremy@appogee.ch>
2025-01-24 18:20:09 +0100

    chore: add all pigs

--- levels/0_tutorial/scenes/characters/animals/scripts/pig1_hit_box.gd
+++ levels/0_tutorial/scenes/characters/animals/scripts/pig1_hit_box.gd
@@ -3,8 +3,14 @@ extends "res://common/hitbox.gd"
 # get level data 
 var level = preload("res://levels/0_tutorial/scripts/level_data.gd").new()
 
+var is_event_triggered = false
+
 func _on_noise_detector_wake_up() -> void:		
-	print("Game over")
+	if !is_event_triggered:
+		is_event_triggered = true
+		
+		print("Game over")
+		
 					
 					
 					
