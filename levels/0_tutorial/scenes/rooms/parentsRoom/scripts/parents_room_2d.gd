c9f09f78c6993b553fa8bc17e0389ef6d60aa9cd
Jeremy Theintz <jeremy@appogee.ch>
2025-01-25 00:46:42 +0100

    style: adjust the I'm hungry animation

--- levels/0_tutorial/scenes/rooms/parentsRoom/scripts/parents_room_2d.gd
+++ levels/0_tutorial/scenes/rooms/parentsRoom/scripts/parents_room_2d.gd
@@ -14,19 +14,10 @@ func _process(delta: float) -> void:
 
 func _on_out_of_room_area_2d_body_entered(body: Node2D) -> void:
 	if !LevelProgess.is_completed(level.name, level.interactions.get_food.key):
-		print("must get_food first")
 		var player = get_tree().get_first_node_in_group("player") as Player
+		var player_animation = player.find_child("AnimatedSprite2D") as AnimatedSprite2D
 			
-		if player:
-			# walk backward
-			player.position = Vector2(player.position.x, player.position.y - 20)
-			
-			
-			var player_animation = player.find_child("AnimatedSprite2D") as AnimatedSprite2D
-			print(player_animation)
-			if player_animation:
-				player_animation.animation = "walk_up"
-			
+		if player and player_animation:
 			var chat_box = player.find_child("ChatBox")
 			
 			if chat_box:
@@ -35,6 +26,11 @@ func _on_out_of_room_area_2d_body_entered(body: Node2D) -> void:
 				
 				player.are_movements_disabled = true
 				await get_tree().create_timer(1.5).timeout
+				
+				# walk backward
+				player_animation.animation = "walk_up"
+				await get_tree().create_timer(0.05).timeout
+				player.position = Vector2(player.position.x, player.position.y - 20)
 				
 				player.are_movements_disabled = false
 				_chat_box._hide_chat_box(chat_box)
