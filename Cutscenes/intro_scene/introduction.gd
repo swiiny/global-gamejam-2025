6fb471075d92346c1b9944fa164b3362a063746b
André Videla <andre.videla@gmail.com>
2025-01-24 16:27:07 +0000

    move to next scene

--- Cutscenes/intro_scene/introduction.gd
+++ Cutscenes/intro_scene/introduction.gd
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func end_cutscene() -> void:
+		get_tree().change_scene_to_file("res://levels/0_tutorial/scenes/Tutorial2D.tscn")
