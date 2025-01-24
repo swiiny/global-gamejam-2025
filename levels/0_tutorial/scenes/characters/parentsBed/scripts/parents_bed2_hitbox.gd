4599fd26951751a68ce6eb153c372e16eb1d62da
Jeremy Theintz <jeremy@appogee.ch>
2025-01-24 10:20:26 +0100

    chore: created separated parent bed logic for the second bed

--- levels/0_tutorial/scenes/characters/parentsBed/scripts/parents_bed2_hitbox.gd
+++ levels/0_tutorial/scenes/characters/parentsBed/scripts/parents_bed2_hitbox.gd
@@ -8,7 +8,7 @@ var is_being_triggered = false
 func _on_noise_detector_wake_up() -> void:		
 	if !is_being_triggered:
 		is_being_triggered = true
-		var parentBed = get_tree().current_scene.find_child("Bed") as Node2D
+		var parentBed = get_tree().current_scene.find_child("Bed2") as Node2D
 		
 		if parentBed:
 			var chat_box = parentBed.find_child("ChatBox") as Panel
