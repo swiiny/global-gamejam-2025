d96cae36e7c5a3f22a8acf9218f223936b7c4486
André Videla <andre.videla@gmail.com>
2025-01-26 01:44:16 +0000

    handle all sorts of inputs

--- menus/main/scripts/main_menu.gd
+++ menus/main/scripts/main_menu.gd
@@ -7,23 +7,30 @@ func _ready() -> void:
 	if Input.get_connected_joypads().size() > 0:
 		control_mode = "gamepad"
 		$VBoxContainer/xbox_controller_label.visible = true
-
 	elif OS.has_feature("mobile"):
 		control_mode = "touch"
+		$VBoxContainer/touch_screen_label.visible = true
 	else:
 		control_mode = "keyboard"
 		$VBoxContainer/StartButton.visible = true
+
 	print("Control Mode: ", control_mode)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_select"):
+		_on_start_button_pressed()
 	pass
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		_on_start_button_pressed()
 
 func _unhandled_input(event):
 	if control_mode == "keyboard":
 		if event is InputEventKey and event.pressed:
 			_on_start_button_pressed()
 			# Add other directions as needed
+
 
 func _on_start_button_pressed() -> void:
 	print("Play button pressed")
