4c957c95995e45410698865293e32b83aaa5bfa1
André Videla <andre.videla@gmail.com>
2025-01-26 09:28:22 +0000

    fix button mashing bug

--- menus/main/scripts/main_menu.gd
+++ menus/main/scripts/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var control_mode = "keyboard"
+var idle = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -35,7 +36,9 @@ func _unhandled_input(event):
 func _on_start_button_pressed() -> void:
 	print("Play button pressed")
 	# Load and change to the game scene
-	var tween = create_tween()
-	tween.tween_property($fade, "modulate:a", 1.0, 3.0) # 1.0 second duration
-	await get_tree().create_timer(3).timeout
-	get_tree().change_scene_to_file("res://levels/0_tutorial/scenes/Tutorial2D.tscn")
+	if idle:
+		idle = false
+		var tween = create_tween()
+		tween.tween_property($fade, "modulate:a", 1.0, 3.0) # 1.0 second duration
+		await get_tree().create_timer(3).timeout
+		get_tree().change_scene_to_file("res://levels/0_tutorial/scenes/Tutorial2D.tscn")
