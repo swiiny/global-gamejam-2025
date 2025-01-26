7fcd0c9918273af26f89a4985aa64c961b42f888
André Videla <andre.videla@gmail.com>
2025-01-26 01:31:24 +0000

    update the main menu

--- menus/main/scripts/main_menu.gd
+++ menus/main/scripts/main_menu.gd
@@ -1,15 +1,29 @@
 extends Control
 
+var control_mode = "keyboard"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if Input.get_connected_joypads().size() > 0:
+		control_mode = "gamepad"
+		$VBoxContainer/xbox_controller_label.visible = true
 
+	elif OS.has_feature("mobile"):
+		control_mode = "touch"
+	else:
+		control_mode = "keyboard"
+		$VBoxContainer/StartButton.visible = true
+	print("Control Mode: ", control_mode)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _unhandled_input(event):
+	if control_mode == "keyboard":
+		if event is InputEventKey and event.pressed:
+			_on_start_button_pressed()
+			# Add other directions as needed
 
 func _on_start_button_pressed() -> void:
 	print("Play button pressed")
